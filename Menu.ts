113dd334cdda14a5138a9ba3a0d8ddee8dff44e0
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2025-11-08 16:19:32 -0300

    adicionando classes do tipo de conta

--- Menu.ts
+++ Menu.ts
@@ -1,12 +1,14 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
 
     let opcao: number;
-
-    // Objeto da Classe Conta (Teste)
+    
+   // Objeto da Classe Conta (Teste)
     const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
     conta.visualizar();
     conta.sacar(10500);
@@ -14,28 +16,42 @@ export function main() {
     conta.depositar(5000);
     conta.visualizar();
 
+   // Objeto da Classe ContaCorrente (Teste)
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
+
     while (true) {
 
-        console.log(colors.bg.black, colors.fg.yellow, 
-                    "*****************************************************");
-        console.log("                                                     ");
-        console.log("                BANCO DO BRAZIL COM Z                ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ");
-        console.log("            1 - Criar Conta                          ");
-        console.log("            2 - Listar todas as Contas               ");
-        console.log("            3 - Buscar Conta por Numero              ");
-        console.log("            4 - Atualizar Dados da Conta             ");
-        console.log("            5 - Apagar Conta                         ");
-        console.log("            6 - Sacar                                ");
-        console.log("            7 - Depositar                            ");
-        console.log("            8 - Transferir valores entre Contas      ");
-        console.log("            9 - Sair                                 ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log(`${colors.bg.black, colors.fg.yellow}
+*****************************************************
+                                                        
+                BANCO DO BRAZIL COM Z                
+                                                        
+*****************************************************
+                                                        
+            1 - Criar Conta                          
+            2 - Listar todas as Contas               
+            3 - Buscar Conta por Numero              
+            4 - Atualizar Dados da Conta             
+            5 - Apagar Conta                         
+            6 - Sacar                                
+            7 - Depositar                            
+            8 - Transferir valores entre Contas      
+            9 - Sair                                 
+                                                        
+*****************************************************${colors.reset}`);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
